d35c86ed21148f143556f683389e70eece2a3b89
riponmondalbd <riponmondal149@gmail.com>
2026-02-07 19:08:50 +0600

    routing and nested routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,34 @@
+import { Route, Routes } from "react-router";
+import BlogTitles from "./pages/BlogTitles";
+import Community from "./pages/Community";
+import Dashboard from "./pages/Dashboard";
+import GenerateImages from "./pages/GenerateImages";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import WriteArticle from "./pages/WriteArticle";
+
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* nested routing for ai */}
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router";
+
 const Layout = () => {
   return (
     <div>
       <h2>layout</h2>
+      <Outlet />
     </div>
   );
 };
