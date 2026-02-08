d825e07e4e2a9c36f31aaf9616873664ad991f40
riponmondalbd <riponmondal149@gmail.com>
2026-02-09 00:41:30 +0600

    trusted peoples and complete the hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import { assets } from "../assets/assets";
 
 const Hero = () => {
   const navigate = useNavigate();
@@ -31,6 +32,12 @@ const Hero = () => {
         <button className="bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer">
           Watch demo
         </button>
+      </div>
+
+      {/* trusted peoples */}
+      <div className="flex  items-center gap-4 mt-8 mx-auto text-gray-600">
+        <img src={assets.user_group} alt="user group" className="h-8" /> Trusted
+        by 10k+ people
       </div>
     </div>
   );
