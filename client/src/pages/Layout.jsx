1d4eaa6d0fa901cb0d0a203ede62e41b28935ebe
riponmondalbd <riponmondal149@gmail.com>
2026-02-09 02:39:22 +0600

    add logo nav menu for mobile device

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,29 @@
-import { Outlet } from "react-router";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { Outlet, useNavigate } from "react-router";
+import { assets } from "../assets/assets";
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false);
+
   return (
-    <div>
-      <h2>layout</h2>
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav>
+        <img
+          src={assets.logo}
+          alt="logo"
+          className="w-32 sm:w-44 cursor-pointer"
+          onClick={() => navigate("/")}
+        />
+
+        {/* menu for mobile device */}
+        {sidebar ? (
+          <X className="w-6 h-6 text-gray-600 sm:hidden" />
+        ) : (
+          <Menu className="w-6 h-6 text-gray-600 sm:hidden" />
+        )}
+      </nav>
       <Outlet />
     </div>
   );
