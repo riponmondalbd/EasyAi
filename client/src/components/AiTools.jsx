8de57781cb4b59c55c6f99deea220cb5fa3ab0a1
riponmondalbd <riponmondal149@gmail.com>
2026-02-09 00:49:38 +0600

    title and description done

---
+++ client/src/components/AiTools.jsx
@@ -0,0 +1,18 @@
+const AiTools = () => {
+  return (
+    <div className="px-4 sm:px-20 xl:px-32 my-24">
+      {/* title and description */}
+      <div className="text-center">
+        <h2 className="text-slate-700 text-[42px] font-semibold">
+          Powerful AI Tools
+        </h2>
+        <p className="text-gray-500 max-w-lg mx-auto">
+          Everything you need to create, enhance, and optimize your content with
+          cutting-edge AI technology.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AiTools;
