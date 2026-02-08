e133725d3d7e3896d44b8b8b872f51400854eb22
riponmondalbd <riponmondal149@gmail.com>
2026-02-09 00:28:48 +0600

    header and description done

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,17 @@
 const Hero = () => {
   return (
-    <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen"></div>
+    <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen">
+      <div className="text-center mb-6">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]">
+          Create amazing content <br /> with{" "}
+          <span className="text-primary">AI tools</span>
+        </h1>
+        <p className="mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600">
+          Transform your content creation with our suite of premium AI tools.
+          Write articles, generate images, and enhance your workflow.
+        </p>
+      </div>
+    </div>
   );
 };
 
