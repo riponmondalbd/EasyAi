323bbaaf37015b755e8c211be488a99ff75d6cec
riponmondalbd <riponmondal149@gmail.com>
2026-02-09 03:15:48 +0600

    protected by real user data

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,3 +1,4 @@
+import { SignIn, useUser } from "@clerk/clerk-react";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { Outlet, useNavigate } from "react-router";
@@ -7,8 +8,9 @@ import Sidebar from "../components/Sidebar";
 const Layout = () => {
   const navigate = useNavigate();
   const [sidebar, setSidebar] = useState(false);
+  const { user } = useUser();
 
-  return (
+  return user ? (
     <div className="flex flex-col items-start justify-start h-screen">
       <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
         <img
@@ -38,6 +40,10 @@ const Layout = () => {
           <Outlet />
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
