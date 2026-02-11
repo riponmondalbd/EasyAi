34104c854b0ae25583e5c2e8c4346cdc1619bb95
riponmondalbd <riponmondal149@gmail.com>
2026-02-11 09:46:23 +0600

    recent creation data show

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Protect } from "@clerk/clerk-react";
 import { Gem, Sparkle } from "lucide-react";
 import { useEffect, useState } from "react";
 import { dummyCreationData } from "../assets/assets";
+import CreationItem from "../components/CreationItem";
 
 const Dashboard = () => {
   const [creations, setCreations] = useState([]);
@@ -11,14 +12,16 @@ const Dashboard = () => {
   };
 
   useEffect(() => {
-    getDashboardData;
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getDashboardData();
   }, []);
 
   return (
     <div className="h-full overflow-y-scroll p-6">
+      {/* cards (total creation & active plan) */}
       <div className="flex justify-start gap-4 flex-wrap">
         {/* total creation card */}
-        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+        <div className="flex justify-between items-center w-full sm:w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
           <div className="text-slate-600">
             <p className="text-sm">Total Creations</p>
             <h2 className="text-xl font-semibold">{creations.length}</h2>
@@ -29,7 +32,7 @@ const Dashboard = () => {
         </div>
 
         {/* active plan card */}
-        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+        <div className="flex justify-between items-center w-full sm:w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
           <div className="text-slate-600">
             <p className="text-sm">Active Plan</p>
             <h2 className="text-xl font-semibold">
@@ -42,6 +45,16 @@ const Dashboard = () => {
             <Gem className="w-5 text-white" />
           </div>
         </div>
+      </div>
+
+      {/* recent creations */}
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+
+        {/* list of creations */}
+        {creations.map((item) => (
+          <CreationItem key={item.id} item={item} />
+        ))}
       </div>
     </div>
   );
