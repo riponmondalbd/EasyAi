01c7fe826cdf1ad86990620f08e2c9c16139f280
riponmondalbd <riponmondal149@gmail.com>
2026-02-14 03:03:50 +0600

    run basic express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: http://localhost:${PORT}`);
+});
