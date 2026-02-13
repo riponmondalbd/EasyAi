a1845d60245fc0cd5dd86f42668e77a5a2c31498
riponmondalbd <riponmondal149@gmail.com>
2026-02-14 03:41:19 +0600

    install and setup clerk to server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
@@ -7,10 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
   res.send("Server is live");
 });
+
+app.use(requireAuth());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: http://localhost:${PORT}`);
