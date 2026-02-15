36d14fc2721822f3adb13a868fa3bb445abf5f91
riponmondalbd <riponmondal149@gmail.com>
2026-02-16 01:38:11 +0600

    blog title generation controller

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -7,6 +7,7 @@ const AI = new OpenAI({
   baseURL: process.env.GEMINI_BASE_URL,
 });
 
+// article generation
 export const generateArticle = async (req, res) => {
   try {
     const { userId } = req.auth();
@@ -37,6 +38,53 @@ export const generateArticle = async (req, res) => {
     const content = response.choices[0].message.content;
 
     await sql` INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},${prompt},${content},'article')`;
+
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// blog title generation
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const plan = req.plan;
+    const free_usage = req.free_usage;
+
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.json({
+        success: false,
+        message: "Limit reached. Upgrade to continue",
+      });
+    }
+
+    // generate article
+    const response = await AI.chat.completions.create({
+      model: process.env.GEMINI_MODEL,
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    await sql` INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},${prompt},${content},'blog-title')`;
 
     if (plan !== "premium") {
       await clerkClient.users.updateUserMetadata(userId, {
