6136635cf0e5720af79caf31932605ed50ce1fca
riponmondalbd <riponmondal149@gmail.com>
2026-02-16 05:03:33 +0600

    controller for toggle like and unlike

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,3 +25,38 @@ export const getPublishedCreations = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// toggle Like Creations
+export const toggleLikeCreations = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+
+    const [creation] = await sql`SELECT * FROM creations WHERE id=${id}`;
+
+    if (!creation) {
+      return res.json({ success: false, message: "Creation not found" });
+    }
+
+    const currentLikes = creation.likes;
+    const userIdStr = userId.toString();
+    let updatedLikes;
+    let message;
+
+    if (currentLikes.includes(userId)) {
+      updatedLikes = currentLikes.filter((user) => user !== userIdStr);
+      message = "Creation Unliked";
+    } else {
+      updatedLikes = [...currentLikes, userIdStr];
+      message = "Creation Liked";
+    }
+
+    const formattedArray = `{${updatedLikes.join(",")}}`;
+
+    await sql` UPDATE creations SET likes= ${formattedArray}::text[] WHERE id=${id}`;
+
+    res.json({ success: true, message });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
